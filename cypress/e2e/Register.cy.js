02ae6ff406ab73f062e43f7534e4505c3497e7fd
DraganaN16 <dragananikolic160685@gmail.com>
2023-05-24 15:38:08 +0200

    test cases

--- cypress/e2e/Register.cy.js
+++ cypress/e2e/Register.cy.js
@@ -14,3 +14,59 @@ describe("Register page", () => {
     cy.wait(3000);
   })
 }); 
+
+describe("Register page", () => {
+  it("First name- empty field", () => {
+    cy.visit("register");
+    cy.get("#last-name").type("Peric");
+    cy.get("#email").type("draganaa@gmail.com");
+    cy.get("#password").type("pokusavam100");
+    cy.get("#password-confirmation").type("pokusavam100");
+    cy.get(".form-check-input").type("checkbox");
+    cy.get(".form-check-label");
+    cy.get("[class='btn btn-custom']").click();
+    cy.wait(3000);
+  })
+}); 
+
+
+describe("Register page", () => {
+  it("Last name - empty field", () => {
+    cy.visit("register");
+    cy.get("#first-name").type("Pera");
+    cy.get("#email").type("draganaa@gmail.com");
+    cy.get("#password").type("pokusavam100");
+    cy.get("#password-confirmation").type("pokusavam100");
+    cy.get(".form-check-input").type("checkbox");
+    cy.get(".form-check-label");
+    cy.get("[class='btn btn-custom']").click();
+    cy.wait(3000);
+  })
+}); 
+
+describe("Register page", () => {
+  it("Empty passwords field", () => {
+    cy.visit("register");
+    cy.get("#first-name").type("Pera");
+    cy.get("#last-name").type("Peric");
+    cy.get("#email").type("draganaa@gmail.com");
+    cy.get(".form-check-input").type("checkbox");
+    cy.get(".form-check-label");
+    cy.get("[class='btn btn-custom']").click();
+    cy.wait(3000);
+  })
+}); 
+
+describe("Register page", () => {
+  it("Not checked terms and conditions", () => {
+    cy.visit("register");
+    cy.get("#first-name").type("Pera");
+    cy.get("#last-name").type("Peric");
+    cy.get("#email").type("draganaa@gmail.com");
+    cy.get("#password").type("pokusavam100");
+    cy.get("#password-confirmation").type("pokusavam100");
+    cy.get(".form-check-label");
+    cy.get("[class='btn btn-custom']").click();
+    cy.wait(3000);
+  })
+}); 
--- cypress/e2e/gallery.cy.js
+++ cypress/e2e/gallery.cy.js
@@ -1,7 +1,7 @@
 /// <reference types='cypress'/>
 
 describe("Home page", () => {
-  it("Home page", () => {
+  it("Home page load", () => {
     cy.visit("https://gallery-app.vivifyideas.com/");
     cy.wait(2000);
   });
