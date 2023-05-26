edf62078a80c09bfa89b04a6e4ff83bb26aa0bf6
DraganaN16 <dragananikolic160685@gmail.com>
2023-05-26 15:02:04 +0200

    commiting

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+//watchForFileChanges: false,
+//defaultCommandTimeout: 3000,
+  setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: "https://gallery-app.vivifyideas.com/",
